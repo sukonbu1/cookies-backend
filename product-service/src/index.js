afe5ed7352d39b6ea74bb8030bfc7f6d407a833d
sukonbu1 <duchuy1912.hn@gmail.com>
2025-06-23 14:35:07 +0700

    fix

--- product-service/src/index.js
+++ product-service/src/index.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+require('dotenv').config({ path: '/home/cookies-backend/product-service/.env' });
 const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
