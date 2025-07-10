9149247e95b4131b6f0400c2ce22218afa6e76ed
sukonbu1 <duchuy1912.hn@gmail.com>
2025-07-10 10:01:53 +0700

    update cors

--- notification-service/src/index.js
+++ notification-service/src/index.js
@@ -10,7 +10,50 @@ const { startConsumer } = require('./consumers/notificationConsumer.js');
 dotenv.config();
 
 const app = express();
-app.use(cors());
+
+const allowedOrigins = [
+  'https://cookies-next-mwpp.vercel.app',
+  'https://cookies2-next.vercel.app',
+  process.env.CORS_ORIGIN || 'http://localhost:3000',
+  'http://localhost:3002',
+  'http://103.253.145.7:3002',
+  'http://localhost:3001',
+  'http://localhost:5173',
+  'http://localhost:8080',
+  'https://localhost:3000',
+  'https://localhost:3001',
+  'https://localhost:5173',
+  'http://localhost:3000',
+  'http://localhost:3001',
+  'http://localhost:5173,',
+  'http://103.253.145.7',
+  'https://103.253.145.7',
+  'http://103.253.145.7:5173',
+];
+
+app.use(cors({
+  origin: function(origin, callback) {
+    if (!origin) {
+      console.log('Request with no origin - allowing');
+      return callback(null, true);
+    }
+    console.log('CORS check for origin:', origin);
+    if (allowedOrigins.indexOf(origin) !== -1 || 
+        origin.startsWith('http://localhost:') || 
+        origin.startsWith('https://localhost:')) {
+      console.log('CORS origin allowed:', origin);
+      return callback(null, true);
+    }
+    console.log('CORS origin not allowed:', origin);
+    return callback(new Error('CORS not allowed'));
+  },
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept', 'Origin'],
+  exposedHeaders: ['Set-Cookie'],
+  preflightContinue: false,
+  optionsSuccessStatus: 204
+}));
 app.use(express.json());
 
 app.use('/api/notifications', notificationRoutes);
