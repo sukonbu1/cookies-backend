b69cf262768ecdb9db276eb9721265fca04965c5
sukonbu1 <duchuy1912.hn@gmail.com>
2025-07-23 15:23:00 +0700

    update

--- post-service/src/consumers/post-events.consumer.js
+++ post-service/src/consumers/post-events.consumer.js
@@ -3,14 +3,21 @@ const rabbitmq = require('../utils/rabbitmq.util');
 const pool = require('../../common/src/config/database');
 
 async function updateCounts(event) {
-  if (event.type === 'post_like') {
-    await pool.query('UPDATE posts SET likes_count = likes_count + 1 WHERE post_id = $1', [event.postId]);
-  } else if (event.type === 'post_unlike') {
-    await pool.query('UPDATE posts SET likes_count = GREATEST(likes_count - 1, 0) WHERE post_id = $1', [event.postId]);
-  } else if (event.type === 'post_create') {
-    await pool.query('UPDATE users SET posts_count = posts_count + 1 WHERE user_id = $1', [event.userId]);
-  } else if (event.type === 'post_delete') {
-    await pool.query('UPDATE users SET posts_count = GREATEST(posts_count - 1, 0) WHERE user_id = $1', [event.userId]);
+  switch (event.type) {
+    case 'post_like':
+      await pool.query('UPDATE posts SET likes_count = likes_count + 1 WHERE post_id = $1', [event.postId]);
+      break;
+    case 'post_unlike':
+      await pool.query('UPDATE posts SET likes_count = GREATEST(likes_count - 1, 0) WHERE post_id = $1', [event.postId]);
+      break;
+    case 'post_create':
+      await pool.query('UPDATE users SET posts_count = posts_count + 1 WHERE user_id = $1', [event.userId]);
+      break;
+    case 'post_delete':
+      await pool.query('UPDATE users SET posts_count = GREATEST(posts_count - 1, 0) WHERE user_id = $1', [event.userId]);
+      break;
+    default:
+      break;
   }
 }
 
